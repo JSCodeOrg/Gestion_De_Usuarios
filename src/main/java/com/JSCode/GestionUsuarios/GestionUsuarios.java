b3103e22b89535227832398e159983afd987f79b
JuanCDiazV <juancamilodiazvalencia66@gmail.com>
2025-03-23 00:00:45 -0500

    configuración base de datos

---
+++ src/main/java/com/JSCode/GestionUsuarios/GestionUsuarios.java
@@ -0,0 +1,12 @@
+package com.JSCode.GestionUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GestionUsuarios {
+    public static void main(String[] args) {
+        SpringApplication.run(GestionUsuarios.class, args);
+    }
+}
