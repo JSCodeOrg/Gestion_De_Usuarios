0402ddf39b3df6969f02d2edb72ae0d4c67a108c
Jhonny <jhony.duque@correounivalle.edu.co>
2025-03-22 21:40:50 -0500

    Configuración de Repositorio

--- src/main/java/com/JSCode/GestionUsuarios/GestionUsuariosApplication.java
+++ src/main/java/com/JSCode/GestionUsuarios/GestionUsuariosApplication.java
@@ -1,4 +1,4 @@
-package com.example.GestionUsuarios;
+package com.JSCode.GestionUsuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,5 @@ public class GestionUsuariosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GestionUsuariosApplication.class, args);
 	}
-
+	
 }
