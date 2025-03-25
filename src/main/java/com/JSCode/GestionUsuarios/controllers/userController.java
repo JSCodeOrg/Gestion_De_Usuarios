392d47d0325fde9f489ed6a24ca2b9f4439369ea
Jhonny <jhony.duque@correounivalle.edu.co>
2025-03-24 19:00:21 -0500

    Creación de entidades de usuario

---
+++ src/main/java/com/JSCode/GestionUsuarios/controllers/userController.java
@@ -0,0 +1,10 @@
+package com.JSCode.GestionUsuarios.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class userController {
+
+}
---
+++ src/main/java/com/JSCode/GestionUsuarios/models/UserPerRole.java
@@ -0,0 +1,28 @@
+package com.JSCode.GestionUsuarios.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users_per_roles")
+public class UserPerRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    private Roles role;
+    
+}
