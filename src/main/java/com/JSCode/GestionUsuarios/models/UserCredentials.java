3a3c8e7ce00121403aaf20b15722c608aaf10294
Jhonny <jhony.duque@correounivalle.edu.co>
2025-03-24 19:44:41 -0500

    Getter y Setter

--- src/main/java/com/JSCode/GestionUsuarios/models/UserCredentials.java
+++ src/main/java/com/JSCode/GestionUsuarios/models/UserCredentials.java
@@ -1,23 +1,12 @@
 package com.JSCode.GestionUsuarios.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserCredentials {
     private String mail;
     private String password;
 
-    // Getters y Setters (IMPORTANTES)
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
