cc8d9944d2791ad3912c2008c323f95540b707c6
Jhonny <jhony.duque@correounivalle.edu.co>
2025-03-26 15:15:44 -0500

    Eliminación de alertas

--- src/main/java/com/JSCode/GestionUsuarios/services/UserService.java
+++ src/main/java/com/JSCode/GestionUsuarios/services/UserService.java
@@ -1,11 +1,8 @@
 package com.JSCode.GestionUsuarios.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
 import com.JSCode.GestionUsuarios.dto.UserRegisterDto;
 import com.JSCode.GestionUsuarios.exceptions.BadRequestException;
 import com.JSCode.GestionUsuarios.exceptions.ConflictException;
